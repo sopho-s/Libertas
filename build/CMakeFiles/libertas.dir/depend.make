edd3ecceb585cfb5a7514818262221a59a77da4a
sopho-s <woodsnicholas01@gmail.com>
2025-12-10 16:10:30 +0000

    forgot to remove build

--- build/CMakeFiles/libertas.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for libertas.
-# This may be replaced when dependencies are built.
--- build/shared/CMakeFiles/argparser.dir/argparser.o.d
+++
@@ -1,4 +0,0 @@
-shared/CMakeFiles/argparser.dir/argparser.o: \
- /home/nick/Documents/Libertas/src/shared/argparser.cpp \
- /usr/include/stdc-predef.h \
- /home/nick/Documents/Libertas/src/shared/argparser.h
